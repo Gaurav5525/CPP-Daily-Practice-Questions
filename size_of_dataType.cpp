1a27d4dc62d190e37f041643383698bbf735a44a
Gaurav Kotwal <gauravkotwal5525@gmail.com>
2022-10-14 13:14:33 +0530

    first commit

---
+++ size_of_dataType.cpp
@@ -0,0 +1,17 @@
+// This program will print size of various data types in bytes
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Size of data type int is "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of data type short is "<<sizeof(short)<<" bytes"<<endl;
+    cout<<"Size of data type float is "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"Size of data type char is "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"Size of data type string is "<<sizeof(string)<<" bytes"<<endl;
+    cout<<"Size of data type double is "<<sizeof(double)<<" bytes"<<endl;
+    cout<<"Size of data type bool is "<<sizeof(bool)<<" bytes"<<endl;
+    cout<<"Size of data type long is "<<sizeof(long)<<" bytes"<<endl;
+
+    return 0;
+}
