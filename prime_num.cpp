21d93a7c55a4d938068f5eb6c4a8346993ba65c1
Gaurav Kotwal <gauravkotwal5525@gmail.com>
2022-10-14 19:50:01 +0530

    second commit

--- prime_num.cpp
+++ prime_num.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int num;
-    cout<<"Enter ANY Number to whether it is PRIME Number or NOT :: ";
+    cout<<"Enter ANY Number to Check whether it is PRIME Number or NOT :: ";
     cin>>num;
 
     if(num%2==0){
