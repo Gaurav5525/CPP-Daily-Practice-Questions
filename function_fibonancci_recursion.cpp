c0a9d6dd9f5df371da30cd0b54263c6d87aecf33
Gaurav Kotwal <gauravkotwal5525@gmail.com>
2022-10-18 17:58:20 +0530

    first commit

---
+++ function_fibonancci_recursion.cpp
@@ -0,0 +1,28 @@
+//This Program will print fibonancci Series using recursion
+#include<iostream>
+using namespace std;
+
+int fibonacci(int num){
+    if(num==0){
+        return 0;
+    }
+    else if(num==1){
+        return 1;
+    }
+    else{
+        return fibonacci(num-1)+fibonacci(num-2);
+    }
+    return 0;
+}
+
+int main(){
+    int num;
+    cout<<"Enter Number till you want FIBONACCI Series :: ";
+    cin>>num;
+
+    for (int i=0; i<num; i++)
+    {
+        cout<<fibonacci(i)<<" ";
+    }
+    return 0;
+}
